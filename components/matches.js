7ee4c01029e9ae28fc7962b6fb9b4ad3193f0814
Macbook Pro <macbookpro@macbooks-mbp.lan>
2024-09-19 17:51:24 -0600

    NS matches

--- components/matches.js
+++ components/matches.js
@@ -11,6 +11,7 @@ export function Matches() {
       try {
         const data = await fetchMatches();
         const matches = data.filter((match) => match.league.round === data[0].league.round);
+        console.log(matches);
         setMatches(matches);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -29,7 +30,9 @@ export function Matches() {
   
   return (
     <div className="grid gap-4 mt-4 mb-20">
-      <h2 className="text-1xl font-bold">{matches[0]?.league.round.replace("-", "- Jornada")}</h2>
+      <h2 className="text-1xl font-bold">
+        {matches[0]?.league.round.replace("-", "- Jornada")}
+      </h2>
       <div className="text-xs text-muted-foreground">
         *Los resultados se actualizan cada hora.
       </div>
@@ -51,12 +54,25 @@ export function Matches() {
             </span>
           </div>
           <div className="text-center flex flex-col items-center w-1/3">
-            <div className="font-bold text-base sm:text-lg">{`${match.goals.home} - ${match.goals.away}`}</div>
-            <div className="text-xs text-muted-foreground">
-              {match.fixture.status.short === "FT"
-                ? "Final"
-                : `${match.fixture.status.elapsed}'`}
-            </div>
+            {match.fixture.status.short != "NS" ? (
+              <>
+                <div className="font-bold text-base sm:text-lg">{`${
+                  match?.goals?.home || 0
+                } - ${match?.goals?.away || 0}`}</div>
+                <div className="text-xs text-muted-foreground">
+                  {match?.fixture?.status?.short === "FT"
+                    ? "Final"
+                    : `${match.fixture.status.elapsed || 0}'`}
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="font-bold text-base sm:text-lg"> vs </div>
+                <div className="text-xs text-muted-foreground">
+                  {match.fixture.venue.name}
+                </div>
+              </>
+            )}
           </div>
           <div className="flex items-center justify-end w-1/3">
             <span className="text-xs sm:text-sm truncate">
